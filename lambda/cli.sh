acfde68d0c16480ed3943583208f62dd307d1b82
dst03106 <dstdbsgml@gmail.com>
2026-01-03 16:53:19 +0900

    chore: add Lambda Docker CLI helper script

---
+++ lambda/cli.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+case "$1" in
+	login)
+		aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+		;;
+  build)
+		docker rmi $(docker images -aq)
+		docker buildx build --platform linux/arm64 --provenance=false -f lambda/Dockerfile -t docker-image:test .
+    ;;
+  run)
+		docker run --platform linux/arm64 -p 9000:8080 --rm --name ic docker-image:test 
+    ;;
+  test)
+		curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d @payload.json                  
+    ;;
+esac
+
